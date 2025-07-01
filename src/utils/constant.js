a11398ac8dc3cad7f9f5c8ccacd06ac9dcf34334
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-07-01 23:52:52 +0530

    Added api key

---
+++ src/utils/constant.js
@@ -0,0 +1,7 @@
+export const API_OPTION = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMGJmZmYzOGUzZGUyYzIzMGQwNDA4MzE0N2U5MzM5YyIsIm5iZiI6MTc0OTQzOTE1MS4zMTQsInN1YiI6IjY4NDY1MmFmZDY2OTIzNWRlMGJiMmY1ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eTewD7H6PjK9ASeq95foxowJrAuJWP76kE1ZJYGPvqA'
+    }
+};
