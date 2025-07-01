5db49225118c47cb760462db8032e36dd4535b98
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-07-01 23:51:58 +0530

    Called tmdb api here

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,4 +1,16 @@
+
+import useGetNowPlaying from "../hooks/useGetNowPlaying";
+import useGetPopular from "../hooks/useGetPopular";
+import useGetTopRated from "../hooks/useGetTopRated";
+import useGetUpcoming from "../hooks/useGetUpcoming";
+
+
 const MainContainer = () => {
+    useGetUpcoming();
+    useGetTopRated();
+    useGetPopular();
+    useGetNowPlaying();
+
     return (
         <div>MainContainer</div>
     )
