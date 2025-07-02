718cc9cbf73619063c49ec9d9c3772b5d0e055fb
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-07-02 23:51:37 +0530

    added css in side bar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -8,16 +8,16 @@ import { CircleUserRound } from 'lucide-react';
 
 const SideBar = () => {
     return (
-        <div className="py-4 px-8 flex flex-col gap-10 w-1/12 h-screen bg-gradient-to-r from-black">
-            <div>
+        <div className="py-4 px-8 flex flex-col gap-10 w-5/12 h-screen bg-gradient-to-r from-black  absolute z-20">
+            <div className="mt-8">
                 <img src="https://img.hotstar.com/image/upload/v1737554969/web-assets/prod/images/rebrand/logo.png" alt="logo" className="h-9 w-9" />
-                <div className="flex flex-col gap-10 mt-14">
-                    <House />
-                    <Search />
-                    <Monitor />
-                    <Popcorn />
-                    <Bike />
-                    <CircleUserRound />
+                <div className="flex flex-col gap-10 mt-28">
+                    <House className="text-white"/>
+                    <Search className="text-white"/>
+                    <Monitor className="text-white"/>
+                    <Popcorn className="text-white"/>
+                    <Bike className="text-white"/>
+                    <CircleUserRound className="text-white"/>
                 </div>
 
             </div>
