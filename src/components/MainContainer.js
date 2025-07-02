e601a9f3517e3330cfc92ea620e647cbf20b54e3
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-07-02 23:50:42 +0530

    added side bar in main container

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -3,16 +3,25 @@ import useGetNowPlaying from "../hooks/useGetNowPlaying";
 import useGetPopular from "../hooks/useGetPopular";
 import useGetTopRated from "../hooks/useGetTopRated";
 import useGetUpcoming from "../hooks/useGetUpcoming";
-
+import SideBar from "./SideBar";
+import VideoBackground from "./VideoBackground";
+import VideoTitle from "./VideoTitle";
 
 const MainContainer = () => {
+
     useGetUpcoming();
     useGetTopRated();
     useGetPopular();
     useGetNowPlaying();
 
     return (
-        <div>MainContainer</div>
+        <div className="scroll">
+            <div className="relative">
+                <VideoBackground />
+                <SideBar />
+                <VideoTitle />
+            </div> 
+        </div>
     )
 }
 
