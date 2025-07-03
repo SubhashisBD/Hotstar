82dc01072f941eb7e9dde7813f4d1bdd310cb19f
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-07-03 22:35:47 +0530

    N*List

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import MovieCard from "./MovieCard";
 
 
-const MovieList = () => {
+const MovieList = ({ title, movies }) => {
 
-   
+    if (!movies) return null;
 
-  return (
-    <div>
-      <MovieList title={"Now Playing"}
-    </div>
-  )
+    return (
+        <div className="px-4 bg-black ml-36 ">
+            <h1 className="text-white py-4 text-3xl font-semibold">{title}</h1>
+            <div className="flex gap-4 w-full overflow-x-auto scroll">
+                {movies?.map((movies) => <MovieCard key={movies.id} poster_path={movies.poster_path} />)}
+            </div>
+
+        </div>
+    )
 }
 
 export default MovieList;
