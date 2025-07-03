b44ebe7106802422c181de56f419aa73a21ad946
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-07-03 22:36:17 +0530

    Diffferent list send to movielist

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
 
 const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies)
 
   return (
-    <div>
-      
+    <div >
+      <div className="bg-black ">
+        <div className="-mt-32">
+          <MovieList title={"Now Playing Movies"} movies={movies.nowPlaying} />
+          <MovieList title={"Popular Movies"} movies={movies.popular} />
+          <MovieList title={"Top Rated Movies"} movies={movies.upcoming} />
+          <MovieList title={"Upcoming Movies"} movies={movies.topRated} />
+        </div>
+
+      </div>
+
     </div>
   )
 }
