d0debb54e87736ade31f35d25b6e813fa400b950
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-07-03 22:35:27 +0530

    Movie card poster

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { IMG_CDN_URL } from "../utils/constant";
 
-const MovieCard = () => {
+const MovieCard = ({poster_path}) => {
+  if(!poster_path) return null;
   return (
-    <div>
-      
+    <div className="">
+      <img className="min-w-48 max-w-48 min-h-60  h- rounded-lg cursor-pointer hover:scale-95 transition-all" alt="Movie Card" src={IMG_CDN_URL+poster_path}/>
     </div>
   )
 }
 
-export default MovieCard
+export default MovieCard;
