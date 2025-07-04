e744370a91be2cfbc06c3afe10ee9317a20fb862
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-07-04 23:53:18 +0530

    Added router path

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,19 +2,39 @@ import React from 'react'
 import MainContainer from "./MainContainer";
 import SideBar from "./SideBar";
 import SecondaryContainer from "./SecondaryContainer";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Search from "./Search";
+
 
 const Body = () => {
-    return (
-        <div >
-            <div className="relative">
-                <MainContainer />
-            </div>
-            <div className=" relative">
-                <SecondaryContainer />
-            </div>
-        </div>
 
-    )
-}
+    const appRouter = createBrowserRouter([
+        {
+            path: "/",
+            element: (
+                <div>
+                    <MainContainer />
+                    <SecondaryContainer />
+                </div>
+            )
+        },
+        {
+            path: "/search",
+            element: <Search />
+        },
+    ])
+    return (
+        <RouterProvider router={appRouter}>
+            <div>
+                <div className="relative">
+                    <MainContainer />
+                </div>
+                <div className="relative">
+                    <SecondaryContainer />
+                </div>
+            </div>
+        </RouterProvider>
+    );
+};
 
 export default Body;
