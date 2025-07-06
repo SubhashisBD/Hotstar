41d973007a22d62a437f5b98bfbcbd1744817eb5
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-07-06 23:05:29 +0530

    Added 4 different Tv data

--- src/utils/movieSlice.js
+++ src/utils/movieSlice.js
@@ -7,7 +7,11 @@ const movieSlice = createSlice(
             nowPlaying: null,
             topRated: null,
             upcoming: null,
-            popular: null
+            popular: null,
+            topRatedTv: null,
+            popularTv: null,
+            onTheAirTv: null,
+            airingTodayTv: null
         },
         reducers: {
             addNowPlaying: (state, action) => {
@@ -21,10 +25,22 @@ const movieSlice = createSlice(
             },
             addPopular: (state, action) => {
                 state.popular = action.payload;
-            }
+            },
+            addTopRatedTv: (state, action) => {
+                state.topRatedTv = action.payload;
+            },
+            addPopularTv: (state, action) => {
+                state.popularTv = action.payload;
+            },
+             addOnTheAirTv: (state, action) => {
+                state.onTheAirTv = action.payload;
+            },
+             addAiringTodayTv: (state, action) => {
+                state.airingTodayTv = action.payload;
+            },
         },
     }
 );
 
-export const { addPopular, addNowPlaying, addTopRated, addUpcoming } = movieSlice.actions;
+export const { addPopular, addNowPlaying, addTopRated, addUpcoming, addTopRatedTv, addPopularTv,addOnTheAirTv,addAiringTodayTv } = movieSlice.actions;
 export default movieSlice.reducer;
